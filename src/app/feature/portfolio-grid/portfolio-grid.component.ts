61b23859dd25174540c52e8742b1de8315a3dc5e
lpachioli <lorenzopachioli@gmail.com>
2022-11-01 14:59:39 -0300

    responcive: portfolio, and contact

--- src/app/feature/portfolio-grid/portfolio-grid.component.ts
+++ src/app/feature/portfolio-grid/portfolio-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
 import { IProject } from '../../shared/interfaces/IProject';
 import { projectsList } from './projectsList';
 
@@ -9,16 +9,49 @@ import { projectsList } from './projectsList';
 })
 export class PortfolioGridComponent implements OnInit {
 
+  innerWidth: any;
+  gridSize = {
+    col: 4,
+    rowHeight: '300px',
+    gutterSize: '20px'
+  }
   projectsList = projectsList;
   @Output() projectChange: EventEmitter<any> = new EventEmitter();
-
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth;
+    this.mobileGrid(this.innerWidth);
+  }
   constructor() { }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    this.mobileGrid(this.innerWidth);
   }
 
   chooseProject(project: IProject): void {
     this.projectChange.emit(project);
   }
 
+  mobileGrid(width: number) {
+    if (width <= 700) {
+      this.gridSize = {
+        col: 2,
+        rowHeight: '200px',
+        gutterSize: '10px'
+      }
+    } else if (width <= 900) {
+      this.gridSize = {
+        col: 3,
+        rowHeight: '250px',
+        gutterSize: '20px'
+      }
+    } else {
+      this.gridSize = {
+        col: 4,
+        rowHeight: '300px',
+        gutterSize: '20px'
+      }
+    }
+  }
 }
