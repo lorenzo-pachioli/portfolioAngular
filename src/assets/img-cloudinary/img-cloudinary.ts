24566eec6e756e9b3a431a13eaa2c3c4a021912d
Lorenzo Pachioli <lorenzopachioli@gmail.com>
2025-09-26 18:04:26 -0300

    cloudinary incorporado e img  borradas

---
+++ src/assets/img-cloudinary/img-cloudinary.ts
@@ -0,0 +1,11 @@
+export const cloudinary = {
+    devsUnited: "https://res.cloudinary.com/dnridopyi/image/upload/v1758918488/devsUnited_rdcckt.png",
+    apiBrainstorm: "https://res.cloudinary.com/dnridopyi/image/upload/v1758918486/api-brainstorming_r3vj9t.png",
+    brainstorm: "https://res.cloudinary.com/dnridopyi/image/upload/v1758918487/brainstormingDescktop_dzpody.png",
+    brainstormMobile: "https://res.cloudinary.com/dnridopyi/image/upload/v1758918488/brainstormingMovile_p9ghnx.png",
+    portfolio: "https://res.cloudinary.com/dnridopyi/image/upload/v1758919045/portfolio2_esfgxr.png",
+    gify: "https://res.cloudinary.com/dnridopyi/image/upload/v1758918491/giphy_w0sjhn.png",
+    rm: "https://res.cloudinary.com/dnridopyi/image/upload/v1758918496/rm-servicios-navales_harkzd.png",
+    apiChat: "https://res.cloudinary.com/dnridopyi/image/upload/v1758918487/apiChat_mduphp.png",
+    chat: "https://res.cloudinary.com/dnridopyi/image/upload/v1758918487/chatApp_l7sn3v.png"
+}
