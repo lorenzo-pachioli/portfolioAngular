7577e67db41c893d59647c7e63beec8a0f7f863f
Lorenzo Pachioli <lorenzopachioli@gmail.com>
2025-09-26 14:00:28 -0300

    pruba cambio env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,3 @@
 export const environment = {
-  production: true,
-  service_id: _NGX_ENV_.NG_APP_SERVICE_ID,
-  template_id: _NGX_ENV_.NG_APP_TEMPLATE_ID,
-  public_key: _NGX_ENV_.NG_APP_PUBLIC_KEY,
-  cv_link: _NGX_ENV_.NG_APP_CVLINK
+  production: true
 };
