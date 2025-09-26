143d7d33198d48af0f1e0021164cefafae97eb72
Lorenzo Pachioli <lorenzopachioli@gmail.com>
2025-09-26 10:36:48 -0300

    traduccion de seccion projects

--- src/app/feature/portfolio-grid/projectsList.ts
+++ src/app/feature/portfolio-grid/projectsList.ts
@@ -8,7 +8,7 @@ export const projectsList: IProject[] = [
     color: 'var(--color4)',
     hrColor: 'var(--color5)',
     image: '../../../assets/img/devsUnited.png',
-    description: 'Its a tweeter alike website using firebase as back-end and React router for the difrent pages. It was created as a evaluated project for the last sprint of the Acamica course.',
+    description: "app.PROJECT.DEVUNITED",
     github: 'https://github.com/lorenzo-pachioli/Devs_united',
     deployed: 'https://devsunited-pachioli.netlify.app'
   },
@@ -19,7 +19,7 @@ export const projectsList: IProject[] = [
     color: 'var(--color4)',
     hrColor: 'var(--color5)',
     image: '../../../assets/img/api-brainstorming.png',
-    description: "It's the server aplication for Brainstorming made in Node with Express and MongoDB.",
+    description: "app.PROJECT.APIBRAIN",
     github: 'https://github.com/lorenzo-pachioli/api-brainstorming',
     deployed: ''
   },
@@ -30,7 +30,7 @@ export const projectsList: IProject[] = [
     color: 'var(--color4)',
     hrColor: 'var(--color5)',
     image: '../../../assets/img/portfolio.png',
-    description: 'Its the web page you are currently visiting. Check out the code in Github to see how it works!',
+    description: "app.PROJECT.PORTFOLIO",
     github: 'https://github.com/lorenzo-pachioli/portfolioAngular',
     deployed: 'https://lorenzo-pachioli.netlify.app/'
   },
@@ -41,7 +41,7 @@ export const projectsList: IProject[] = [
     color: 'var(--color4)',
     hrColor: 'var(--color5)',
     image: '../../../assets/img/giphy.png',
-    description: 'Its a static SPA that conects to the API of Giphy (https://giphy.com/) and alows the user to search gifs throw a simple searcher.It was created as a evaluated project for the third sprint of the Acamica course.',
+    description: "app.PROJECT.GIFY",
     github: 'https://github.com/lorenzo-pachioli/Sprint_proyect_Giphy',
     deployed: 'https://sprint-proyect-giphy.netlify.app'
   },
@@ -52,7 +52,7 @@ export const projectsList: IProject[] = [
     color: 'var(--color4)',
     hrColor: 'var(--color5)',
     image: '../../../assets/img/brainstormingDescktop.png',
-    description: "It's a scrum base task manager created as a final project for the Lamansys bootcamp using Angular and Angular Material, conecting to an API provided by them and following some requirements.",
+    description: "app.PROJECT.BRAIN",
     github: 'https://github.com/lorenzo-pachioli/frontend-bootcamp',
     deployed: 'https://frontend-bootcamp-pachioli.netlify.app'
   },
@@ -63,7 +63,7 @@ export const projectsList: IProject[] = [
     color: 'var(--color4)',
     hrColor: 'var(--color5)',
     image: '../../../assets/img/rm-servicios-navales.png',
-    description: "It's a landing page created for a client dedicated to commercial ship's mechanics",
+    description: "app.PROJECT.RM",
     github: 'https://github.com/lorenzo-pachioli/RMservicios-navales',
     deployed: 'https://rm-servicios-navales.netlify.app/'
   },
@@ -74,7 +74,7 @@ export const projectsList: IProject[] = [
     color: 'var(--color4)',
     hrColor: 'var(--color5)',
     image: '../../../assets/img/apiChat.png',
-    description: "It's the server aplication for Chat-app made in Node with Socket.io, Express and MongoDB.",
+    description: "app.PROJECT.APICHAT",
     github: 'https://github.com/lorenzo-pachioli/Api-chat',
     deployed: ''
   },
@@ -85,7 +85,7 @@ export const projectsList: IProject[] = [
     color: 'var(--color4)',
     hrColor: 'var(--color5)',
     image: '../../../assets/img/chatApp.png',
-    description: "It's a real time chat web site that uses React and Socket.io-client to communicate with Api-chat.",
+    description: "app.PROJECT.CHAT",
     github: 'https://github.com/lorenzo-pachioli/Chat-app',
     deployed: 'https://pachioli-chat-app.netlify.app'
   },
