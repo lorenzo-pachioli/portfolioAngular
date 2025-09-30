41b355efc57f0e1ba3602616416286c5a391a77e
Lorenzo Pachioli <lorenzopachioli@gmail.com>
2025-09-30 09:29:18 -0300

    correccion lenguaje default Y prueba en entorno

--- src/app/feature/language-selector/language-selector.component.ts
+++ src/app/feature/language-selector/language-selector.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/services/language/language.service';
 
 @Component({
   selector: 'app-language-selector',
@@ -8,14 +9,25 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class LanguageSelectorComponent {
 
-	language = localStorage.getItem('lang');
+  language = localStorage.getItem('lang');
 
-  constructor(public translate: TranslateService) {
+  constructor(public translate: TranslateService, private langService: LanguageService) {
+  }
+
+  ngAfterViewInit() {
+    const INIT_LANG = this.langService.initLang();
+    console.log("init", this.language, INIT_LANG);
+    if(this.language){
+      this.translate.use(this.language);
+    } else {
+      this.translate.use(INIT_LANG);
+    }
+     
   }
 
   setLang(lang: string): void {
-		this.language = lang;
-		this.translate.use(lang);
-		localStorage.setItem('lang', lang);
-	}
+    this.language = lang;
+    this.translate.use(lang);
+    localStorage.setItem('lang', lang);
+  }
 }
